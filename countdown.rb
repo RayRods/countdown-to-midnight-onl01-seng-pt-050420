6a1673d297d9b28be6f290aad438e65f9a7643f2
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-15 02:59:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,8 +5,15 @@ def countdown(number)
     while number > 0 
      puts "#{number} SECOND(S)!"
       number -= 1 
-      sleep(5)
     end
-  
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+   number = 10
+    while number > 10
+     puts "#{number} SECOND(S)!"
+      number -= 10
+    end 
+    puts "HAPPY NEW YEAR!"
+  end
