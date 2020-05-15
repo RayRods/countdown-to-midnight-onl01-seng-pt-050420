2d5fa112f7708c1c9ce11b2fbbe72278fd7cafe9
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-15 02:55:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown
-  count = 10
-    while count > 0 
-     puts "#{count} SECOND(S)!"
-      count -= 1 
+def countdown(number)
+  number = 10
+    while number > 0 
+     puts "#{number} SECOND(S)!"
+      number -= 1 
     end
   sleep 5 
 end
